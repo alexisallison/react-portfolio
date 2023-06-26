b85224fc9206715bfdeddb232f3b69fab0906443
alexisallison <114777162+alexisallison@users.noreply.github.com>
2023-06-25 20:36:44 -0400

    added portfolio compinent and installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-
+import Portfolio from "./Portfolio.js"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-Hello World        </p>
-        
-      </header>
+   <Portfolio />
     </div>
   );
 }
