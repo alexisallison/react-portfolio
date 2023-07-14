35bcf8b9d56adde70707fd9a435130ec715a52a0
alexisallison <114777162+alexisallison@users.noreply.github.com>
2023-07-13 21:01:59 -0400

    started installing icons from font awesome

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "./images/logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 import "./styles/Navbar.css";
+
 
  function Navbar(){
     return(
         <nav className="Navbar container">
 <img src={Logo} alt="Logo" className="logo" />
+<button className="hamburger"><FontAwesomeIcon icon={faBars} /></button>
             <ul>
                 <li>
                     <Link to="/Home" className="navlink">Home</Link>
