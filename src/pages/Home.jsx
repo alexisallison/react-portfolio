43462729bccc0823b76adfc95bdd4715e3c5f4d6
alexisallison <114777162+alexisallison@users.noreply.github.com>
2023-07-14 21:48:16 -0400

    some styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,62 +24,62 @@ function Home() {
         <div className="col d-none d-lg-block">
           <img
             src={travel}
-            class="project-image img-fluid"
+            className="project-image img-fluid"
             alt="HTML Project"
           />
         </div>
-        <div class="col">
-          <div class="project-description">
-            <h2 class="mb-5">Travel Landing Page</h2>
-            <p class="mb-4">
+        <div className="col">
+          <div className="project-description">
+            <h2 className="mb-5">Travel Landing Page</h2>
+            <p className="mb-4">
               Landing page for a travel site to The Maldives. This project
               utilized HTML, CSS and JavaScript with a focus on mastering iframe.
             </p>
             <a
               href="/work.html#travel"
-              class="btn btn-branding-outline"
+              className="btn btn-branding-outline"
               >Learn More</a
             >
           </div>
         </div>
       </div>
-      <div class="row mb-5">
-        <div class="col">
-          <div class="project-description">
-            <h2 class="mb-5">Weather App Project</h2>
-            <p class="mb-4">
+      <div className="row mb-5">
+        <div className="col">
+          <div className="project-description">
+            <h2 className="mb-5">Weather App Project</h2>
+            <p className="mb-4">
               App using HTML, CSS, Javascript, Bootstrap, Weather API and
               Location API to search for any city and display the current
               temperature and forecast for the next 5 days.
             </p>
-            <a href="/work.html#weather-app" class="btn btn-branding-outline"
+            <a href="/work.html#weather-app" className="btn btn-branding-outline"
               >Learn More</a
             >
           </div>
         </div>
-        <div class="col d-none d-lg-block">
+        <div className="col d-none d-lg-block">
           <img
             src={weather}
-            class="project-image img-fluid"
+            className="project-image img-fluid"
             alt="Weather App"
           />
         </div>
       </div>
-      <div class="row mb-5">
-        <div class="col d-none d-lg-block">
+      <div className="row mb-5">
+        <div className="col d-none d-lg-block">
           <img
             src={dictionary}
-            class="project-image img-fluid"
+            className="project-image img-fluid"
             alt="Dictionary App"
           />
         </div>
-        <div class="col">
-          <div class="project-description">
-            <h2 class="mb-5">Dictionary Project</h2>
-            <p class="mb-4">
+        <div className="col">
+          <div className="project-description">
+            <h2 className="mb-5">Dictionary Project</h2>
+            <p className="mb-4">
               React app using HTML, CSS, Bootstrap, Javascript, and two API's to search for any word and display the definitions, synonyms, examples and images.
             </p>
-            <a href="/work.html#dictionary" class="btn btn-branding-outline"
+            <a href="/work.html#dictionary" className="btn btn-branding-outline"
               >Learn More</a
             >
           </div>
